8a04dbfe33d33583d6fc1bf0470b95c08c173c5a
zztrieuzz <trieu19@gmail.com>
2022-01-05 10:46:12 +0700

    fix #46

--- lua/spectre/ui.lua
+++ lua/spectre/ui.lua
@@ -145,6 +145,34 @@ function M.render_header(opts)
     utils.write_virtual_text(state.bufnr, config.namespace_header, 0, {{ help_text, 'Comment' } })
 end
 
+---@private
+--- Creates autocommands to close a preview window when events happen.
+---
+---@param events table list of events
+---@param winnr number window id of preview window
+---@param bufnrs table list of buffers where the preview window will remain visible
+---@see |autocmd-events|
+local function close_preview_autocmd(events, winnr, bufnrs)
+  local augroup = 'preview_window_'..winnr
+
+  -- close the preview window when entered a buffer that is not
+  -- the floating window buffer or the buffer that spawned it
+  vim.cmd(string.format([[
+    augroup %s
+      autocmd!
+      autocmd BufEnter * lua vim.lsp.util._close_preview_window(%d, {%s})
+    augroup end
+  ]], augroup, winnr, table.concat(bufnrs, ',')))
+
+  if #events > 0 then
+    vim.cmd(string.format([[
+      augroup %s
+        autocmd %s <buffer> lua vim.lsp.util._close_preview_window(%d)
+      augroup end
+    ]], augroup, table.concat(events, ','), winnr))
+  end
+end
+
 M.show_menu_options = function (title, content)
     local win_width, win_height = vim.lsp.util._make_floating_popup_size(content,{})
     local help_win, preview = popup.create(content, {
@@ -159,11 +187,11 @@ M.show_menu_options = function (title, content)
     })
 
     vim.api.nvim_win_set_option(help_win, 'winblend', 0)
-    vim.lsp.util.close_preview_autocmd({"CursorMoved", "CursorMovedI", "BufHidden", "BufLeave"},
-        preview.border.win_id
+    close_preview_autocmd({"CursorMoved", "CursorMovedI", "BufHidden", "BufLeave"},
+        preview.border.win_id,{preview.bufnr}
     )
-    vim.lsp.util.close_preview_autocmd({"CursorMoved", "CursorMovedI", "BufHidden", "BufLeave"},
-        help_win
+    close_preview_autocmd({"CursorMoved", "CursorMovedI", "BufHidden", "BufLeave"},
+        help_win,{preview.bufnr}
     )
 end
 
