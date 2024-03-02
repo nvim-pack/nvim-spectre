98edb5676331b0061eb5b01739c8df4046d07b71
Nurdaulet Kurenshe <nurdaulet.kurenshe@med365.kz>
2024-03-02 23:04:02 +0600

    Make tab configurable

--- lua/spectre/config.lua
+++ lua/spectre/config.lua
@@ -35,6 +35,16 @@ local config = {
         replace = 'SpectreReplace',
     },
     mapping = {
+        ['tab'] = {
+            map = '<Tab>',
+            cmd = "<cmd>lua require('spectre').tab()<cr>",
+            desc = 'next query'
+        },
+        ['shift-tab'] = {
+            map = '<S-Tab>',
+            cmd = "<cmd>lua require('spectre').tab_shift()<cr>",
+            desc = 'previous query'
+        },
         ['toggle_line'] = {
             map = 'dd',
             cmd = "<cmd>lua require('spectre').toggle_line()<CR>",
