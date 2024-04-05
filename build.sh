f37b8c6640876c921a5074484f0ff967b938f06f
Maneren <49210777+Maneren@users.noreply.github.com>
2024-04-06 00:59:24 +0200

    respect $CARGO_TARGET_DIR

--- build.sh
+++ build.sh
@@ -6,14 +6,14 @@ cd spectre_oxi
 
 cargo build --release
 
+[ "$CARGO_TARGET_DIR" = "" ] && CARGO_TARGET_DIR=target
+
 if [ "$(uname)" == "Darwin" ]; then
-    cp target/release/libspectre_oxi.dylib ../lua/spectre_oxi.so
+    cp "$CARGO_TARGET_DIR"/release/libspectre_oxi.dylib ../lua/spectre_oxi.so
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    cp target/release/libspectre_oxi.so ../lua/spectre_oxi.so
+    cp "$CARGO_TARGET_DIR"/release/libspectre_oxi.so ../lua/spectre_oxi.so
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-    cp target/release/libspectre_oxi.dll ../lua/spectre_oxi.dll
+    cp "$CARGO_TARGET_DIR"/release/libspectre_oxi.dll ../lua/spectre_oxi.dll
 fi
 rm -rf target
 echo "Build Done"
-
-
