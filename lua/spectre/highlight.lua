cdd90d4276cc1738ac540d8b46fd61b3307ccb17
xieyong <qxieyongp@163.com>
2025-02-28 21:16:20 +0800

    feat add default to highlight group definition.
    param `default = true` will be honored to user's pre-define highlight
    groups, which will be friendly for pre-defined colorshemes.

--- lua/spectre/highlight.lua
+++ lua/spectre/highlight.lua
@@ -1,13 +1,13 @@
 local M = {}
 
 M.set_hl = function()
-    vim.api.nvim_set_hl(0, 'SpectreHeader', { link = 'Comment' })
-    vim.api.nvim_set_hl(0, 'SpectreBody', { link = 'String' })
-    vim.api.nvim_set_hl(0, 'SpectreFile', { link = 'Keyword' })
-    vim.api.nvim_set_hl(0, 'SpectreDir', { link = 'Comment' })
-    vim.api.nvim_set_hl(0, 'SpectreSearch', { link = 'DiffChange' })
-    vim.api.nvim_set_hl(0, 'SpectreBorder', { link = 'Comment' })
-    vim.api.nvim_set_hl(0, 'SpectreReplace', { link = 'DiffDelete' })
+    vim.api.nvim_set_hl(0, 'SpectreHeader', { link = 'Comment', default = true })
+    vim.api.nvim_set_hl(0, 'SpectreBody', { link = 'String', default = true })
+    vim.api.nvim_set_hl(0, 'SpectreFile', { link = 'Keyword', default = true })
+    vim.api.nvim_set_hl(0, 'SpectreDir', { link = 'Comment', default = true })
+    vim.api.nvim_set_hl(0, 'SpectreSearch', { link = 'DiffChange', default = true })
+    vim.api.nvim_set_hl(0, 'SpectreBorder', { link = 'Comment', default = true })
+    vim.api.nvim_set_hl(0, 'SpectreReplace', { link = 'DiffDelete', default = true })
 end
 
 vim.api.nvim_create_autocmd('ColorScheme', {
